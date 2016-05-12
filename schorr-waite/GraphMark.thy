2beb30ba280ee4ba252b05f84b7bc11a79c981d7
Matthew Brecknell <matthew@brecknell.net>
2016-05-12 15:10:01 +1000

    more graph marking specification

--- schorr-waite/GraphMark.thy
+++ schorr-waite/GraphMark.thy
@@ -21,23 +21,28 @@ definition out :: "lifted_globals \<Rightarrow> node_C ptr \<Rightarrow> node_C 
 definition mark :: "bool \<Rightarrow> node_C \<Rightarrow> node_C" where
   "mark c node \<equiv> if c then node \<lparr> mark_C := 3 \<rparr> else node"
 
-definition mark_pre :: "state_pred \<Rightarrow> node_C ptr \<Rightarrow> state_pred" where
-  "mark_pre P root \<equiv> \<lambda> s. let R = reach (out s) {root} in
+definition mark_precond :: "state_pred \<Rightarrow> node_C ptr \<Rightarrow> state_pred" where
+  "mark_precond P root \<equiv> \<lambda> s. let R = reach (out s) {root} in
     (\<forall> p \<in> R. ok p s \<and> s[p]\<rightarrow>mark = 0) \<and> P (heap_node_C_update (\<lambda> h p. mark (p \<in> R) (h p)) s)"
-
-primrec mark_invariant :: "state_pred \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<times> node_C ptr \<Rightarrow> state_pred" where
-  "mark_invariant P root (p,q) s = True"
 
 primrec roots :: "node_C ptr \<times> node_C ptr \<Rightarrow> node_C ptr set" where
   "roots (p,q) = { r \<in> {p,q}. r \<noteq> NULL }"
 
+primrec mark_path :: "state_pred \<Rightarrow> node_C ptr list \<Rightarrow> state_pred" where
+  "mark_path P [] s = True" |
+  "mark_path P (p # ps) s =
+    (s[p]\<rightarrow>mark = 1 \<and> True)"
+
+primrec mark_invariant :: "state_pred \<Rightarrow> node_C ptr \<times> node_C ptr \<Rightarrow> state_pred" where
+  "mark_invariant P (p,q) s = (\<exists> path. mark_path P path s)"
+
 primrec mark_measure :: "(node_C ptr \<times> node_C ptr) \<times> lifted_globals \<Rightarrow> nat" where
   "mark_measure (r,s) = setsum (\<lambda> p. 3 - unat s[p]\<rightarrow>mark) (reach (out s) (roots r))"
 
-lemma graph_mark'_correct: "\<lbrace> mark_pre P root \<rbrace> graph_mark' root \<lbrace> \<lambda> _. P \<rbrace>!"
+lemma graph_mark'_correct: "\<lbrace> mark_precond P root \<rbrace> graph_mark' root \<lbrace> \<lambda> _. P \<rbrace>!"
   unfolding
-    graph_mark'_def mark_pre_def
-    whileLoop_add_inv[where I="mark_invariant P root" and M="mark_measure"]
+    graph_mark'_def mark_precond_def
+    whileLoop_add_inv[where I="mark_invariant P" and M="mark_measure"]
   apply (wp; clarsimp)
   sorry
 
