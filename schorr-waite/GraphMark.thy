6a0106d324898012070ad8bcdfc973016a0bab8a
Matthew Brecknell <matthew@brecknell.net>
2016-05-17 07:53:32 +1000

    schorr-waite: a more likely invariant

--- schorr-waite/GraphMark.thy
+++ schorr-waite/GraphMark.thy
@@ -28,46 +28,48 @@ definition mark_precondition :: "state_pred \<Rightarrow> node_C ptr \<Rightarro
 primrec roots :: "node_C ptr \<times> node_C ptr \<Rightarrow> node_C ptr set" where
   "roots (p,q) = { r \<in> {p,q}. r \<noteq> NULL }"
 
-type_synonym path_ok_t =
-  "lifted_globals \<Rightarrow> lifted_globals \<Rightarrow> node_C ptr set
-    \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr list \<Rightarrow> bool "
+type_synonym path_upd_t =
+  "lifted_globals \<Rightarrow> node_C ptr set
+    \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr list \<Rightarrow> lifted_globals option"
 
-primrec tail_ok :: path_ok_t where
-  "tail_ok s t M r q p [] = (p = NULL \<and> q = r)" |
-  "tail_ok s t M r q p (p' # ps) =
-    (p = p' \<and>
-      ((t[p]\<rightarrow>mark = 1 \<and>
-          t[p]\<rightarrow>left = q \<and> t[p]\<rightarrow>right = s[p]\<rightarrow>left \<and> tail_ok s t M r p s[p]\<rightarrow>right ps) \<or>
-       (t[p]\<rightarrow>mark = 2 \<and> t[p]\<rightarrow>left \<in> M \<and>
-          t[p]\<rightarrow>right = q \<and> t[p]\<rightarrow>left = s[p]\<rightarrow>right \<and> tail_ok s t M r p s[p]\<rightarrow>left ps)))"
+primrec tail_upd :: path_upd_t where
+  "tail_upd s M r q p [] = (if p = NULL \<and> q = r then Some s else None)" |
+  "tail_upd s M r q p (p' # ps) =
+   (if p \<noteq> p' then None
+    else if s[p]\<rightarrow>mark = 1 then
+      tail_upd s[p\<rightarrow>left := q][p\<rightarrow>right := s[p]\<rightarrow>left] M r p s[p]\<rightarrow>right ps
+    else if s[p]\<rightarrow>mark = 2 \<and> s[p]\<rightarrow>right \<in> M then
+      tail_upd s[p\<rightarrow>right := q][p\<rightarrow>left := s[p]\<rightarrow>right] M r p s[p]\<rightarrow>left ps
+    else None)"
 
-primrec path_ok :: path_ok_t where
-  "path_ok s t M r q p [] = (p = NULL \<and> q = r)" |
-  "path_ok s t M r q p (p' # ps) =
-    (p = p' \<and>
-      ((t[p]\<rightarrow>mark = 0 \<and>
-          t[p]\<rightarrow>left = s[p]\<rightarrow>left \<and> t[p]\<rightarrow>right = s[p]\<rightarrow>right) \<or>
-       (t[p]\<rightarrow>mark = 1 \<and> t[p]\<rightarrow>left \<in> M \<and>
-          t[p]\<rightarrow>left = q \<and> t[p]\<rightarrow>right = s[p]\<rightarrow>left \<and> tail_ok s t M r p s[p]\<rightarrow>right ps) \<or>
-       (t[p]\<rightarrow>mark = 2 \<and> t[p]\<rightarrow>left \<in> M \<and> t[p]\<rightarrow>right \<in> M \<and>
-          t[p]\<rightarrow>right = q \<and> t[p]\<rightarrow>left = s[p]\<rightarrow>right \<and> tail_ok s t M r p s[p]\<rightarrow>left ps)))"
+primrec path_upd :: path_upd_t where
+  "path_upd s M r q p [] = (if p = NULL \<and> q = r then Some s else None)" |
+  "path_upd s M r q p (p' # ps) =
+   (if p \<noteq> p' then None
+    else if s[p]\<rightarrow>mark = 0 then
+      tail_upd s M r p q ps
+    else if s[p]\<rightarrow>mark = 1 \<and> q \<in> M then
+      tail_upd s[p\<rightarrow>left := q][p\<rightarrow>right := s[p]\<rightarrow>left] M r p s[p]\<rightarrow>right ps
+    else if s[p]\<rightarrow>mark = 2 \<and> s[p]\<rightarrow>right \<in> M \<and> q \<in> M then
+      tail_upd s[p\<rightarrow>right := q][p\<rightarrow>left := s[p]\<rightarrow>right] M r p s[p]\<rightarrow>left ps
+    else None)"
 
 primrec mark_invariant :: "state_pred \<Rightarrow> node_C ptr \<Rightarrow> node_C ptr \<times> node_C ptr \<Rightarrow> state_pred" where
   "mark_invariant P root (p,q) s =
-    (\<exists> t path M.
+    (\<exists> t u path M.
       let
         R = reach (out t) {root};
         U = R - M;
         F = U - set path
       in
         P t \<and>
-        M = reach (out t) M \<and>
-        (\<forall> p \<in> R. ptr_ok p t) \<and>
-        (\<forall> p \<in> M. s[p]\<rightarrow>mark = 3) \<and>
-        (\<forall> p \<in> F. s[p]\<rightarrow>mark = 0) \<and>
-        path_ok s t M root q p path \<and>
+        Some u = path_upd s M root q p path \<and>
+        t = heap_node_C_update (\<lambda> h p. mark (p \<in> U) (h p)) u \<and>
         R = reach (out s) (roots (p,q)) \<and>
-        t = heap_node_C_update (\<lambda> h p. mark (p \<in> U) (h p)) s)"
+        M = reach (out t) M \<and>
+        (\<forall> p \<in> R. ptr_ok p s) \<and>
+        (\<forall> p \<in> M. s[p]\<rightarrow>mark = 3) \<and>
+        (\<forall> p \<in> F. s[p]\<rightarrow>mark = 0))"
 
 primrec mark_measure :: "(node_C ptr \<times> node_C ptr) \<times> lifted_globals \<Rightarrow> nat" where
   "mark_measure (r,s) = setsum (\<lambda> p. 3 - unat s[p]\<rightarrow>mark) (reach (out s) (roots r))"
@@ -77,9 +79,12 @@ lemma graph_mark'_correct: "\<lbrace> mark_precondition P root \<rbrace> graph_m
     graph_mark'_def mark_precondition_def
     whileLoop_add_inv[where I="mark_invariant P root" and M="mark_measure"]
   apply (wp; clarsimp)
-  subgoal for p q s t path M sorry
-  subgoal for q s t path M sorry
-  subgoal for s sorry
+  subgoal for p q s t u path M
+   sorry
+  subgoal for q s t u path M
+   sorry
+  subgoal for s
+   sorry
   done
 
 end
